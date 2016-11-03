7846ff851be225e5d03facb19f68910040b6c507
Garazi <greg@assortedgarbage.com>
2016-11-03 13:26:55 -0700

    Removed comments

--- metadata/aura/QuickProspect/QuickProspectController.js
+++ metadata/aura/QuickProspect/QuickProspectController.js
@@ -1,25 +1,6 @@
 ({
     doInit: function(component, event, helper) {
-    	var foo = component.get("v.sObjectName");
-    	console.log('quick foo: ', foo);
-    	var property_id = component.get("v.recordId");
-    	console.log('bar: ', property_id)
-        // // Prepare a new record from template
-        // component.find("ProspectRecordCreator").getNewRecord(
-        //     "Prospect__c", // sObject type (entity API name)
-        //     null, // record type
-        //     null, // default record values
-        //     false, // skip cache?
-        //     $A.getCallback(function() {
-        //         var rec = component.get("v.newProspect");
-        //         var error = component.get("v.newProspectError");
-        //         if (error || (rec === null)) {
-        //             console.log("Error initializing record template: " + error);
-        //         } else {
-        //             console.log("Record template initialized: " + rec.sobjectType);
-        //         }
-        //     })
-        // );
+    	console.log("Component initialized");
     },
 
     handleSaveProspect: function(component, event, helper) {
@@ -41,10 +22,8 @@
 
         action.setCallback(this, function(response) {
             var res = response.getReturnValue();
-            console.log("foo: ", res);
             var resultsToast = $A.get("e.force:showToast");
             if (res) {
-                // Success! Prepare a toast UI message              
                 resultsToast.setParams({
                     "title": "Prospect Saved",
                     "message": "The new prospect was created."
